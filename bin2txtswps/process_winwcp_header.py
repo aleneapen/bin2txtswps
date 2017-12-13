1676f164aba2ddd3eaccd2abcd152b87ac943d29
aleneapen <alen@eapen.uk>
2017-12-13 17:31:48 +0000

    bug fix

--- bin2txtswps/process_winwcp_header.py
+++ bin2txtswps/process_winwcp_header.py
@@ -12,22 +12,14 @@ def process_header(header_list):
     for header_info in header_list:
         try:
             key,value = header_info.strip("\r").split("=")
-
-            # Check if the number of channels is more than 1
-            if key == "NC" and value != 1:
-                HB_size = ((int(value)-1/8) + 1) * 1024
             header_dict.update({key:value})
         except ValueError as e:
             pass
-    return header_dict,HB_size
+    return header_dict
 
 def read_bin_header(file_name_wcp,HB_size = 1024):
     # First read 1024 bytes and process header:
-    header_dict, HB_size = process_header(get_header_list(file_name_wcp))
-
-    # Check if header size is enough:
-    # if HB_size!=1024:
-    #     header_dict,_ = process_header(get_header_list(file_name_wcp,HB_size=HB_size))
+    header_dict = process_header(get_header_list(file_name_wcp))
 
     return header_dict
 
